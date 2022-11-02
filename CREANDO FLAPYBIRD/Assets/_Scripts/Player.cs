2022aa8a2a1a4a33b3190b8802dca1f87ef935e7
Miguel <miguel.carrenog@my.unitec.edu.mx>
2022-11-02 16:07:08 -0600

    UnituProyectFlappyBird
    Add UnityFolder

---
+++ CREANDO FLAPYBIRD/Assets/_Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+Rigidbody _rigidbody;
+ public float force;
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space)) 
+       {
+              _rigidbody.AddForce(Vector3.up * force, ForceMode.Impulse);
+       }
+    }
+}
